5ae3ad94be9f298783656b947080120bea23f9bf
Adam Logan <adamlogan42@gmail.com>
2026-05-30 17:15:14 +0100

    Add active field for player

--- src/pool_tracker/Player.py
+++ src/pool_tracker/Player.py
@@ -1,6 +1,7 @@
-from mongoengine import Document, StringField, IntField
+from mongoengine import Document, StringField, IntField, BooleanField
 
 class Player(Document):
     _minimum_elo = 100
     name = StringField(required=True, unique=True)
     elo = IntField(required=True, min_value=_minimum_elo, default = 400)
+    active = BooleanField(required=True, default=True)
