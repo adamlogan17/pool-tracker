95e2dd0c52a70462313d73ab5a94303416f0bbc8
Adam Logan <adamlogan42@gmail.com>
2026-06-04 19:41:05 +0100

    Add to_dict functions for the classes

--- src/pool_tracker/Match.py
+++ src/pool_tracker/Match.py
@@ -1,10 +1,8 @@
 from datetime import datetime, timezone
-
+import json
 from mongoengine import Document, IntField, ReferenceField, DateTimeField, signals
 from pool_tracker.elo import update_elo
 from pool_tracker.Player import Player
-
-
 
 class Match(Document):
     winning_player = ReferenceField(Player, required=True)
@@ -22,5 +20,15 @@ class Match(Document):
         update_elo(document.winning_player, document.losing_player)
         document.winning_player_elo_after = document.winning_player.elo
         document.losing_player_elo_after = document.losing_player.elo
+
+    def to_dict(self):
+        json_str = self.to_json()
+        match_dict = json.loads(json_str)
+        match_dict['id'] = match_dict['_id']['$oid']
+        del match_dict['_id']
+        match_dict['match_time'] = match_dict['match_time']['$date']
+        match_dict['winning_player'] = Player.objects(id=match_dict['winning_player']['$oid']).first().to_dict()
+        match_dict['losing_player'] = Player.objects(id=match_dict['losing_player']['$oid']).first().to_dict()
+        return match_dict
 
 signals.pre_save.connect(Match.pre_save, sender=Match)
--- src/pool_tracker/Player.py
+++ src/pool_tracker/Player.py
@@ -1,7 +1,15 @@
 from mongoengine import Document, StringField, IntField, BooleanField
+import json
 
 class Player(Document):
     _minimum_elo = 100
     name = StringField(required=True, unique=True)
     elo = IntField(required=True, min_value=_minimum_elo, default = 400)
     active = BooleanField(required=True, default=True)
+    
+    def to_dict(self):
+        json_str = self.to_json()
+        player_dict = json.loads(json_str)
+        player_dict['id'] = player_dict['_id']['$oid']
+        del player_dict['_id']
+        return player_dict
